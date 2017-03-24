d7e389a58f65d7af0a8f3542985007b317df5a64
Antonio Cárdenas <toni_acg@hotmail.com>
2017-03-24 17:40:15 +0100

    dd

--- src/WebApiCore/Controllers/ValuesController.cs
+++ src/WebApiCore/Controllers/ValuesController.cs
@@ -9,14 +9,14 @@ namespace WebApiCore.Controllers
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
-        {
+        { 
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
-        {
+        { 
             return "value";
         }
 
